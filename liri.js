3ad79cae22e792495751c1fe8eafe2dbc5da7dc0
Andrew Dicer <andrew.dicer@gmail.com>
2017-07-12 19:15:58 -0400

    imbd done

--- liri.js
+++ liri.js
@@ -1,6 +1,7 @@
 var keys = require("./keys.js");
 var Twitter = require('twitter');
 var Spotify = require('node-spotify-api');
+var request = require('request');
 
 // console.log(keys.spotifyKeys.id);
 
@@ -34,25 +35,40 @@ var Spotify = require('node-spotify-api');
 
 //------------------------------- Spotify -----------------------------------------
 
-	var spotify = new Spotify({
-	  id: keys.spotifyKeys.id,
-	  secret: keys.spotifyKeys.secret,
-	});
+	// var spotify = new Spotify({
+	//   id: keys.spotifyKeys.id,
+	//   secret: keys.spotifyKeys.secret,
+	// });
 
-	console.log("Spotify");
+	// console.log("Spotify");
 
-	spotify.search({ type: 'track', query: 'the sign',}, function(err, data) {
-  		if (err) {
-    		return console.log('Error occurred: ' + err);
-  		}
-  	var info = data.tracks.items;
+	// spotify.search({ type: 'track', query: 'the sign',}, function(err, data) {
+ //  		if (err) {
+ //    		return console.log('Error occurred: ' + err);
+ //  		}
+ //  	var info = data.tracks.items;
 
- 	for (var i = 0; i < info.length; i++){
- 	console.log("-----------");
-	console.log(info[i].name);
-	console.log(info[i].artists[0].name)
-	} 
-	});
+ // 	for (var i = 0; i < info.length; i++){
+ // 	console.log("-----------");
+	// console.log(info[i].name);
+	// console.log(info[i].artists[0].name)
+	// } 
+	// });
 
 //------------------------------- imbd ---------------------------------------------
 
+var queryUrl = "http://www.omdbapi.com/?t=Tron&y=&plot=short&apikey=40e9cece";
+
+	request(queryUrl, function(error, response, body) {
+	  	if (!error && response.statusCode === 200) {
+	  		console.log("Title: " + JSON.parse(body).Title);
+	   		console.log("Release Year: " + JSON.parse(body).Year);
+	   		console.log("IMBD Rating: " + JSON.parse(body).imdbRating);
+	   		console.log("Rotten Tomatoes Rating: " + JSON.parse(body).Ratings[1].Value);
+	   		console.log("Country: " + JSON.parse(body).Country);
+	   		console.log("Language: " + JSON.parse(body).Language);
+	   		console.log("Plot: " + JSON.parse(body).Plot);
+	   		console.log("Actors: " + JSON.parse(body).Actors);
+
+	  	}
+	});
