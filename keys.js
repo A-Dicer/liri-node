b12cf7153050a6648e0de1ef76d6c572c11090b0
Andrew Dicer <andrew.dicer@gmail.com>
2017-07-12 19:00:21 -0400

    spotify done

--- keys.js
+++ keys.js
@@ -6,3 +6,8 @@ exports.twitterKeys = {
   access_token_key: '885224216005685251-AuWhQ5L8688KBYjr76AODctlwdtvn1T',
   access_token_secret: '1LVeqYUbcq8kVIVFOUnx5M98WRR827j1zYdvLKv4OFeol',
 }
+
+exports.spotifyKeys = {
+	id: '2a7d8524667f45c992a554ba75d0ecd2',
+	secret: '409319f5a39e44fc8e3836445fc89305',
+}
--- liri.js
+++ liri.js
@@ -1,32 +1,58 @@
 var keys = require("./keys.js");
 var Twitter = require('twitter');
+var Spotify = require('node-spotify-api');
 
-// console.log(keys.twitterKeys.consumer_key);
+// console.log(keys.spotifyKeys.id);
 
+//------------------------------- Twitter -----------------------------------------
 
+// var client = new Twitter({
+//   consumer_key: keys.twitterKeys.consumer_key,
+//   consumer_secret: keys.twitterKeys.consumer_secret,
+//   access_token_key: keys.twitterKeys.access_token_key,
+//   access_token_secret: keys.twitterKeys.access_token_secret,
+// });
  
-var client = new Twitter({
-  consumer_key: keys.twitterKeys.consumer_key,
-  consumer_secret: keys.twitterKeys.consumer_secret,
-  access_token_key: keys.twitterKeys.access_token_key,
-  access_token_secret: keys.twitterKeys.access_token_secret,
-});
- 
-var params = {screen_name: 'AndrewDicer'};
-client.get('statuses/user_timeline', params, function(error, tweets, response) {
-  if (!error) {
-  	console.log("");
-  	console.log("Twitter");
-  	console.log("-------------------");
+// var params = {screen_name: 'AndrewDicer'};
+// client.get('statuses/user_timeline', params, function(error, tweets, response) {
+//   if (!error) {
+//   	console.log("");
+//   	console.log("Twitter");
+//   	console.log("-------------------");
 
-  	for (var i = 0; i < tweets.length && i < 3; i++){
-  		var text = tweets[i].text;
-  		var created = tweets[i].created_at;
-    	console.log(created);
-    	console.log(text);
-    	console.log("-------------------");
-	}
-	console.log("");
-  }
-  else {console.log(error)};
-});
+//   	for (var i = 0; i < tweets.length && i < 20; i++){
+//   		var text = tweets[i].text;
+//   		var created = tweets[i].created_at;
+//     	console.log(created);
+//     	console.log(text);
+//     	console.log("-------------------");
+// 	}
+// 	console.log("");
+//   }
+//   else {console.log(error)};
+// });
+
+//------------------------------- Spotify -----------------------------------------
+
+	var spotify = new Spotify({
+	  id: keys.spotifyKeys.id,
+	  secret: keys.spotifyKeys.secret,
+	});
+
+	console.log("Spotify");
+
+	spotify.search({ type: 'track', query: 'the sign',}, function(err, data) {
+  		if (err) {
+    		return console.log('Error occurred: ' + err);
+  		}
+  	var info = data.tracks.items;
+
+ 	for (var i = 0; i < info.length; i++){
+ 	console.log("-----------");
+	console.log(info[i].name);
+	console.log(info[i].artists[0].name)
+	} 
+	});
+
+//------------------------------- imbd ---------------------------------------------
+
